6f32b4c7e077986f57bf40c0a3cb900b6585f623
hideaki_takikawa <tkkw0411@gmail.com>
2021-07-16 12:19:04 +0900

    mapping 統合

--- part2_日本語コーパスを使用していろいろな検索方法を試す/web-api/app.py
+++ part2_日本語コーパスを使用していろいろな検索方法を試す/web-api/app.py
@@ -20,16 +20,21 @@ def index():
 def analyzer():
     query = request.args.get('q')
 
-    body_ngram = {
+    body = {
         "query" : {
-            "match" : {
-                "content": {
-                "query": query,
-                "operator": "or",
-                "fuzziness" : "AUTO",
-                "prefix_length" : 1,
-                "boost": 1.3
-                }
+            "function_score": {
+            "query": {
+                "bool": {"should": [
+                {"match": {"content_ma": {
+                    "query": "オーズ",
+                    "boost": 0.5
+                }}},
+                {"match": {"content": {
+                    "query": "ライダー オーズ",
+                    "boost": 2
+                }}}
+                ]}
+            }
             }
         },
         "_source" : ["_score", "id", "content"],
@@ -37,7 +42,7 @@ def analyzer():
         "size": 1000
     }
 
-    res = es.search(index="imdtablema", body=body_ngram)
+    res = es.search(index="imdtable", body=body)
     tmp_list = []
     id_list = {}
     count = 0
@@ -49,38 +54,7 @@ def analyzer():
         id_list[hit['_source']['id']] = count
         count += 1
         tmp_list.append(s_dict)
-
-    body_ma = {
-        "query" : {
-            "match" : {
-                "content": {
-                "query": query,
-                "operator": "or",
-                "fuzziness" : "AUTO",
-                "prefix_length" : 1
-                }
-            }
-        },
-        "_source" : ["_score", "id", "content"],
-        "from": 0,
-        "size": 1000
-    }
-
-    res = es.search(index="imdtablengram", body=body_ma)
-    for hit in res['hits']['hits']:
-        if hit['_source']['id'] in id_list.keys():
-            tmp_list[id_list[hit['_source']['id']]]["score"] = tmp_list[id_list[hit['_source']['id']]]["score"] + hit['_score']
-        else :
-            s_dict = {}
-            s_dict["id"] = hit['_source']['id']
-            s_dict["score"] = hit['_score']
-            s_dict["content"] = hit['_source']['content']
-            id_list[hit['_source']['id']] = count
-            count += 1
-            tmp_list.append(s_dict)
-    # return jsonify(res)
     print(query)
-    #res_merge = json.dumps({"hits":sorted(tmp_list, key=lambda x: x['score'], reverse=True)}, indent=2, ensure_ascii=False)
     return jsonify({"hits":sorted(tmp_list, key=lambda x: x['score'], reverse=True)})
 
 
